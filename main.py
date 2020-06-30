082b38c79f70a49525f5384b2a7a65a098e5a45e
yebh17 <sunnyrebels0@gmail.com>
2020-06-30 11:23:49 +0200

    ML-02: Add webserver for hosting the application

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 import logging
+from flask import Flask
 
+main = Flask(__name__)
 
 logging.basicConfig(filename='/var/log/app.log', filemode='a', format='%(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-a = 2
-b = 0
-try:
-   c = a/b
-   if c != 0:
-   	print = logging.info("c is not equal to 0!!", exc_info=True)
-   else:
-       print = logging.critical("c is equal to 0!", exc_info=True)
-except Exception as e:
-  logging.error("denominator must not be zero", exc_info=True)
-   
+
+@main.route('/')
+
+def return_div():
+  a = 2
+  b = 2
+  c = a/b
+
+  try:
+    if c != 0:
+      print = logging.info("c is not equal to 0!", exc_info=True)
+      return str(c)
+    else:
+      print = logging.error("c is equal to 0!", exc_info=True)
+      return str(c)
+  except ZeroDivisionError as error:
+    print = logging.critical("denominator must not be zero", exc_info=True)
 #logging.debug('This is a debug message')
 #logging.info('This is an info message')
 #logging.warning('This is a warning message')
 #logging.error('This is an error message')
 #logging.critical('This is a critical message')
+
+if __name__ == '__main__':
+    main.run(debug=True,host='0.0.0.0')
