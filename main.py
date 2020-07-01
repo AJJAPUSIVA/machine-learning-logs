5fa3d0ada0e6774b2a78157dbd17b6d14bc7edec
sroopsai <roopsai.work@gmail.com>
2020-07-01 20:08:48 +0200

    Major Changes

--- main.py
+++ main.py
@@ -1,35 +1,37 @@
 import random
 import logging
 from flask import Flask
+from typing import List
 
 main = Flask(__name__)
 
-logging.basicConfig(filename='/var/log/app.log', filemode='a', format='%(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logging.basicConfig(filename='/var/log/app.log', filemode='a', format='%(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
 
 @main.route('/')
-def return_div():
-  numlist = []
-  for x in range(10):
-    a = random.randint(1,30)
-    b = 6
-    c = a/b
-    numlist.append(c)
+def division():
+    results: List = []
+    for i in range(10):
+        try:
+            num1 = random.randint(0, 30)
+            num2 = random.randint(0, 30)
+            result = num1 / num2
+            if result == 0:
+                logging.error("result is equal to zero!", exc_info=True)
+            else:
+                logging.info("result is not equal to zero!", exc_info=True)
+                results.append(result)
+        except ZeroDivisionError:
+            logging.critical("Denominator must not be Zero!", exc_info=True)
+    return str(results)
 
 
-  try:
-    if c != 0:
-      print = logging.info("c is not equal to 0!", exc_info=True)
-      return  str(numlist)
-    else:
-      print = logging.error("c is equal to 0!", exc_info=True)
-      return  str(numlist)
-  except ZeroDivisionError as error:
-    print = logging.critical("denominator must not be zero", exc_info=True)
-#logging.debug('This is a debug message')
-#logging.info('This is an info message')
-#logging.warning('This is a warning message')
-#logging.error('This is an error message')
-#logging.critical('This is a critical message')
+# logging.debug('This is a debug message')
+# logging.info('This is an info message')
+# logging.warning('This is a warning message')
+# logging.error('This is an error message')
+# logging.critical('This is a critical message')
 
 if __name__ == '__main__':
-    main.run(debug=True,host='0.0.0.0')
+    main.run(debug=True, host='0.0.0.0')
