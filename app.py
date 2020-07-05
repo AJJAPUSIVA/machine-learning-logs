c430767efb6d016ec39f90969f0bf9cf833cc973
yebh17 <sunnyrebels0@gmail.com>
2020-07-05 11:10:43 +0200

    ML-06: Add logging to the sign up flow

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, redirect, url_for, request
+import logging
 
 app = Flask(__name__)
+logging.basicConfig(filename='/var/log/app.log', filemode='a', format='%(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+                    
 # Route for handling the login page logic
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -8,8 +11,10 @@ def login():
     if request.method == 'POST':
         if request.form['username'] != 'admin' or request.form['password'] != 'admin':
             error = 'Invalid Credentials. Please try again.'
+            logging.error("Invalid Credentials!", exc_info=True)
         else:
-            return redirect(url_for('home'))
+            logging.info("success!", exc_info=True)
+            return redirect('https://i.pinimg.com/originals/78/ac/aa/78acaad3c2890c0d47f94ec7b3cce9fb.jpg')
     return render_template('login.html', error=error)
 
 if __name__ == '__main__':
